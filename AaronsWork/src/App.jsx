2f1d1f82e1050e8e9d314f79cc665246b4fba98f
Aaron Allen <aaronseth.allen@gmail.com>
2024-07-24 05:17:49 -0400

    Started work on content

--- AaronsWork/src/App.jsx
+++ AaronsWork/src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='d-flex '>
+      <header className="d-flex justify-content-start">
+        <h1 className='d-flex' >Aaron Allen</h1>
+        <Nav />
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ AaronsWork/src/components/Nav.jsx
@@ -0,0 +1,21 @@
+export default function Nav(){
+
+    return(
+        <section className="d-flex flex-row">
+            <nav >
+                <div>
+                <a href="#">Portfolio</a>
+                </div>
+                <div>
+                <a href="#">About</a>
+                </div>
+                <div>
+                <a href="#">Contact</a>
+                </div>
+                <div>
+                <a href="#">Resume</a>
+                </div>
+            </nav>
+        </section>
+    );
+}
